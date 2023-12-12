8eb8aadb2253f4d45213cbd387a1f5682f5e82f0
Rikard radovac <rikard.radovac@gmail.com>
2023-12-12 14:46:43 +0100

    fix prompts

--- rag/prompts.py
+++ rag/prompts.py
@@ -1,10 +1,10 @@
 
 
-prompt_template_llama = """System prompt: You answer questions in a short, concise and informative way. I ask you a question and you answer it. You only answer the question without repeating the question. The answer is only based on the following context: {context}. The answ
+prompt_template_llama = """System prompt: You answer questions in a short, concise and informative way. I ask you a question and you answer it. You only answer the question without repeating the question. The answer is only based on the following context: {context}.
 question: {question}
 """
 
 
-prompt_template_mistral = """<s>[INST]System prompt: You answer questions in a short, concise and informative way. I ask you a question and you answer it. You only answer the question without repeating the question. The answer is only based on the following context: {context}. The answ
+prompt_template_mistral = """<s>[INST]System prompt: You answer questions in a short, concise and informative way. I ask you a question and you answer it. You only answer the question without repeating the question. The answer is only based on the following context: {context}.
 question: {question} [/INST]
 """
